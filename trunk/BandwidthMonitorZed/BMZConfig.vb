329b0f883b130e100c94cc5c700a2827fd2f9e0c
shawarmakarma@gmail.com <shawarmakarma@gmail.com@522d3376-4ff9-737a-bd60-6c5e05e1c01d>
2010-10-28 16:56:50 +0000

    Getting started with the Config Form

---
+++ trunk/BandwidthMonitorZed/BMZConfig.vb
@@ -0,0 +1,49 @@
+﻿Public Class BMZConfig
+    Private DisplayInBytes_ As Boolean = False
+    Property DisplayInBytes As Boolean
+        Get
+            Return DisplayInBytes_
+        End Get
+        Set(ByVal value As Boolean)
+            If DisplayInBytes_ <> value Then
+                DisplayInBytes_ = value
+                RaiseEvent DisplayInBytesChanged(Me, New System.EventArgs)
+            End If
+        End Set
+    End Property
+    Event DisplayInBytesChanged(ByVal sender As Object, ByVal e As System.EventArgs)
+
+    Private KiloIs1024_ As Boolean = False
+    Property KiloIs1024 As Boolean
+        Get
+            Return KiloIs1024_
+        End Get
+        Set(ByVal value As Boolean)
+            If KiloIs1024_ <> value Then
+                KiloIs1024_ = value
+                RaiseEvent KiloIs1024Changed(Me, New System.EventArgs)
+            End If
+        End Set
+    End Property
+    Event KiloIs1024Changed(ByVal sender As Object, ByVal e As System.EventArgs)
+
+    Public Enum DisplayYAxisType
+        None
+        Max
+        Scale
+    End Enum
+
+    Private YAxisStyle_ As DisplayYAxisType = DisplayYAxisType.Scale
+    Property YAxisStyle() As DisplayYAxisType
+        Get
+            Return YAxisStyle_
+        End Get
+        Set(ByVal value As DisplayYAxisType)
+            If value <> YAxisStyle_ Then
+                YAxisStyle_ = value
+                RaiseEvent YAxisStyleChanged(Me, New System.EventArgs)
+            End If
+        End Set
+    End Property
+    Event YAxisStyleChanged(ByVal sender As Object, ByVal e As System.EventArgs)
+End Class
---
+++ trunk/BandwidthMonitorZed/ConfigForm.vb
@@ -0,0 +1,43 @@
+﻿Public Class ConfigForm
+    Dim config As BMZConfig
+
+    Public Sub New(ByVal config As BMZConfig)
+        ' This call is required by the designer.
+        InitializeComponent()
+        ' Add any initialization after the InitializeComponent() call.
+        Me.config = config
+    End Sub
+
+    Private Sub ConfigForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        LoadFromConfig()
+    End Sub
+
+    Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+        SaveToConfig()
+    End Sub
+
+    Private Sub SaveToConfig()
+        config.DisplayInBytes = chkDisplayInBytes.Checked
+        config.KiloIs1024 = chkKiloIs1024.Checked
+        If radYAxisMax.Checked Then config.YAxisStyle = BMZConfig.DisplayYAxisType.Max
+        If radYAxisNone.Checked Then config.YAxisStyle = BMZConfig.DisplayYAxisType.None
+        If radYAxisScale.Checked Then config.YAxisStyle = BMZConfig.DisplayYAxisType.Scale
+    End Sub
+
+    Private Sub LoadFromConfig()
+        chkDisplayInBytes.Checked = config.DisplayInBytes
+        chkKiloIs1024.Checked = config.KiloIs1024
+        Select Case config.YAxisStyle
+            Case BMZConfig.DisplayYAxisType.Max
+                radYAxisMax.Checked = True
+            Case BMZConfig.DisplayYAxisType.None
+                radYAxisNone.Checked = True
+            Case BMZConfig.DisplayYAxisType.Scale
+                radYAxisScale.Checked = True
+        End Select
+    End Sub
+End Class
